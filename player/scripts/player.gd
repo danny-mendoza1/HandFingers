aef0e10a168c67d1bdc003227a8c569d5cf1d443
Danny <d.mendoza875@gmail.com>
2025-01-28 22:57:14 -0600

    removes finite state machine adds component based architecture, refactors fire delay timer to use built in timer node

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,23 +1,29 @@
 class_name Player extends CharacterBody2D
+
+@export var movement_component: MovementComponent
+@export var attack_component: AttackComponent
+
 var direction: Vector2 = Vector2.ZERO
-@onready var state_machine: Node = $StateMachine
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	state_machine.Initialize(self)
-	pass  # Replace with function body.
+	if movement_component == null:
+		movement_component = $MovementComponent
+	if attack_component == null:
+		attack_component = $AttackComponent
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	direction = Vector2(Input.get_axis("player_left", "player_right"), Input.get_axis("player_up", "player_down")).normalized()
-	pass
+
+	# Handle movement and attacking simultaneously
+	movement_component.move(direction, delta)
+	attack_component.attack(delta)
 
 
-func _physics_process(_delta):
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
-	#Make sure we are in the screen
+	# Ensure the player stays within the screen bounds
 	var view_rect: Rect2 = get_viewport_rect()
 	position.x = clamp(position.x, 0, view_rect.size.x)
 	position.y = clamp(position.y, 0, view_rect.size.y)
